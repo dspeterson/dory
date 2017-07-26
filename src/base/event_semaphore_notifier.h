504b5af68d5e1b4a5cb5c7f5e1fb8583d9d37128
Dave Peterson <dave@dspeterson.com>
2017-07-26 08:17:49 -0700

    add TEventSemaphoreNotifier class

---
+++ src/base/event_semaphore_notifier.h
@@ -0,0 +1,61 @@
+/* <base/event_semaphore_notifier.h>
+
+   ----------------------------------------------------------------------------
+   Copyright 2017 Dave Peterson <dave@dspeterson.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   ----------------------------------------------------------------------------
+
+   RAII event semaphore notifier class.  Destructor makes sure semaphore push
+   operation has occurred.
+ */
+
+#pragma once
+
+#include <functional>
+#include <system_error>
+
+#include <base/event_semaphore.h>
+#include <base/no_copy_semantics.h>
+
+namespace Base {
+
+  class TEventSemaphoreNotifier {
+    NO_COPY_SEMANTICS(TEventSemaphoreNotifier);
+
+    public:
+    using TErrorHandler =
+        std::function<void(const std::system_error &) noexcept>;
+
+    TEventSemaphoreNotifier(TEventSemaphore &sem,
+        const TErrorHandler &error_handler);
+
+    /* This class supports subclassing so that a subclass can pass its own
+       error handler in the constructor.  The alternative of creating a wrapper
+       class that provides the error handler is a bit more verbose. */
+    virtual ~TEventSemaphoreNotifier() noexcept {
+      Notify();
+    }
+
+    void Notify() noexcept;
+
+    private:
+    bool Done;
+
+    TEventSemaphore &Sem;
+
+    TErrorHandler ErrorHandler;
+  };  // TEventSemaphoreNotifier
+
+}  // Base
+
---
+++ src/base/event_semaphore_notifier.test.cc
@@ -0,0 +1,96 @@
+/* <base/event_semaphore_notifier.test.cc>
+ 
+   ----------------------------------------------------------------------------
+   Copyright 2017 Dave Peterson <dave@dspeterson.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   ----------------------------------------------------------------------------
+ 
+   Unit test for <base/event_semaphore_notifier.h>.
+ */
+
+#include <base/event_semaphore_notifier.h>
+
+#include <cstdlib>
+#include <iostream>
+  
+#include <base/event_semaphore.h>
+  
+#include <gtest/gtest.h>
+  
+using namespace Base;
+
+namespace {
+
+  /* The fixture for testing class TEventSemaphoreNotifier. */
+  class TEventSemaphoreNotifierTest : public ::testing::Test {
+    protected:
+    TEventSemaphoreNotifierTest() {
+    }
+
+    virtual ~TEventSemaphoreNotifierTest() {
+    }
+
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+  };  // TEventSemaphoreNotifierTest
+
+  TEST_F(TEventSemaphoreNotifierTest, Test1) {
+    Base::TEventSemaphore sem;
+    ASSERT_FALSE(sem.GetFd().IsReadable());
+
+    {
+      TEventSemaphoreNotifier n(sem,
+          [](const std::system_error &x) {
+            std::cerr << "Event semaphore push error: " << x.what()
+                << std::endl;
+            exit(1);
+          }
+      );
+    }
+
+    ASSERT_TRUE(sem.GetFd().IsReadable());
+    sem.Pop();
+    ASSERT_FALSE(sem.GetFd().IsReadable());
+  }
+
+  TEST_F(TEventSemaphoreNotifierTest, Test2) {
+    Base::TEventSemaphore sem;
+    ASSERT_FALSE(sem.GetFd().IsReadable());
+
+    {
+      TEventSemaphoreNotifier n(sem,
+          [](const std::system_error &x) {
+            std::cerr << "Event semaphore push error: " << x.what()
+                << std::endl;
+            exit(1);
+          }
+      );
+
+      n.Notify();
+    }
+
+    ASSERT_TRUE(sem.GetFd().IsReadable());
+    sem.Pop();
+    ASSERT_FALSE(sem.GetFd().IsReadable());
+  }
+  
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
