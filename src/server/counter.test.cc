a0ee65206feb64a9fcf8df3963d8f19d0970274f
Dave Peterson <dave@dspeterson.com>
2019-09-15 23:14:52 -0700

    remove dependency of Server::TCounter on stuff in socket directory

--- src/server/counter.test.cc
+++ src/server/counter.test.cc
@@ -24,20 +24,22 @@
 #include <cstdio>
 #include <cstring>
 #include <thread>
-#include <unistd.h>
-#include <sys/epoll.h>
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <base/error_util.h>
 #include <base/fd.h>
-#include <base/os_error.h>
 #include <base/zero.h>
-#include <socket/address.h>
 
 #include <gtest/gtest.h>
 
-using namespace std;
 using namespace Base;
 using namespace Server;
-using namespace Socket;
 
 namespace {
 
@@ -46,21 +48,21 @@ namespace {
 
   const size_t BufSize = 1024;
 
-  void ServerMain(int die, int sock, bool &success) {
+  static void ServerMain(int die, int sock, bool *success) {
     try {
-      success = true;
+      *success = true;
       TFd ep(epoll_create1(0));
       epoll_event event;
       Zero(event);
       event.data.fd = die;
       event.events = EPOLLIN;
-      TOsError::IfLt0(HERE, epoll_ctl(ep, EPOLL_CTL_ADD, die, &event));
+      IfLt0(epoll_ctl(ep, EPOLL_CTL_ADD, die, &event));
       event.data.fd = sock;
       event.events = EPOLLIN;
-      TOsError::IfLt0(HERE, epoll_ctl(ep, EPOLL_CTL_ADD, sock, &event));
+      IfLt0(epoll_ctl(ep, EPOLL_CTL_ADD, sock, &event));
 
       for (; ; ) {
-        TOsError::IfLt0(HERE, epoll_wait(ep, &event, 1, -1));
+        IfLt0(epoll_wait(ep, &event, 1, -1));
 
         if (event.data.fd == die) {
           break;
@@ -73,50 +75,64 @@ namespace {
           for (; ; ) {
             char buf[BufSize];
             ssize_t size;
-            TOsError::IfLt0(HERE, size = read(cli, buf, BufSize));
+            IfLt0(size = read(cli, buf, BufSize));
 
             if (!size) {
               break;
             }
 
             Requests.Increment();
-            TOsError::IfLt0(HERE, write(cli, buf, size));
+            IfLt0(write(cli, buf, size));
           }
         }
       }
     } catch (...) {
-      success = false;
+      *success = false;
     }
   }
 
-  static void ClientMain(int id, const TAddress &address,
-      uint32_t request_count, bool &success) {
+  static void ClientMain(int id, in_port_t port, uint32_t request_count,
+      bool *success) {
     try {
-      success = true;
-      TFd my_socket(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP));
-      Connect(my_socket, address);
+      *success = true;
+      TFd my_socket(socket(AF_INET, SOCK_STREAM, 0));
+      sockaddr_in addr;
+      Zero(addr);
+      addr.sin_family = AF_INET;
+      addr.sin_port = htons(port);
+      addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+      IfLt0(connect(my_socket, reinterpret_cast<sockaddr *>(&addr),
+          sizeof(addr)));
 
       for (uint32_t i = 0; i < request_count; ++i) {
         char request[BufSize];
         sprintf(request, "%d %d", id, i);
         ssize_t request_size = strlen(request);
         ssize_t size;
-        TOsError::IfLt0(HERE, size = write(my_socket, request, request_size));
+        IfLt0(size = write(my_socket, request, request_size));
 
         if (size != request_size) {
           throw 0;
         }
 
         char reply[BufSize];
-        TOsError::IfLt0(HERE, size = read(my_socket, reply, BufSize));
+        IfLt0(size = read(my_socket, reply, BufSize));
 
         if ((size != request_size) || strncmp(request, reply, request_size)) {
           throw 0;
         }
       }
     } catch (...) {
-      success = false;
+      *success = false;
     }
+  }
+
+  in_port_t GetBindPort(int listening_socket) {
+    struct sockaddr_in addr;
+    socklen_t addrlen = sizeof(addr);
+    IfLt0(getsockname(listening_socket,
+        reinterpret_cast<struct sockaddr *>(&addr), &addrlen));
+    return ntohs(addr.sin_port);
   }
 
   /* The fixture for testing counters. */
@@ -135,30 +151,36 @@ namespace {
 
   TEST_F(TCounterTest, Typical) {
     int fds[2];
-    TOsError::IfLt0(HERE, pipe(fds));
+    IfLt0(pipe(fds));
     TFd recv_die(fds[0]), send_die(fds[1]);
-    TFd listening_socket(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP));
-    Bind(listening_socket, TAddress(TAddress::IPv4Loopback));
-    TOsError::IfLt0(HERE, listen(listening_socket, 5));
-    TAddress address = GetSockName(listening_socket);
+    TFd listening_socket(socket(AF_INET, SOCK_STREAM, 0));
+    sockaddr_in server_addr;
+    Zero(server_addr);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = 0;  // bind() to ephemeral port
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    IfLt0(bind(listening_socket,
+        reinterpret_cast<const sockaddr *>(&server_addr), sizeof(server_addr)));
+    const in_port_t bind_port = GetBindPort(listening_socket);
+    IfLt0(listen(listening_socket, 5));
     bool server_success = false,
         client_1_success = false,
         client_2_success = false,
         client_3_success = false;
     static const uint32_t request_count = 5;
-    thread
-        server(ServerMain, static_cast<int>(recv_die),
-               static_cast<int>(listening_socket), ref(server_success)),
-        client_1(ClientMain, 101, cref(address), request_count,
-                 ref(client_1_success)),
-        client_2(ClientMain, 102, cref(address), request_count,
-                 ref(client_2_success)),
-        client_3(ClientMain, 103, cref(address), request_count,
-                 ref(client_3_success));
+    std::thread
+        server(&ServerMain, static_cast<int>(recv_die),
+               static_cast<int>(listening_socket), &server_success),
+        client_1(&ClientMain, 101, bind_port, request_count,
+            &client_1_success),
+        client_2(&ClientMain, 102, bind_port, request_count,
+            &client_2_success),
+        client_3(&ClientMain, 103, bind_port, request_count,
+            &client_3_success);
     client_1.join();
     client_2.join();
     client_3.join();
-    TOsError::IfLt0(HERE, write(send_die, "x", 1));
+    IfLt0(write(send_die, "x", 1));
     server.join();
     ASSERT_TRUE(server_success);
     ASSERT_TRUE(client_1_success);
